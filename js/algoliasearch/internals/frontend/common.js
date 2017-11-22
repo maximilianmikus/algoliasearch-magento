391c6903ecad6859b01d7f666ff26f5a23d5e926
Maximilian Mikus <maximilianmikus@gmail.com>
2017-11-23 00:49:31 +0100

    Add special_price_to logic. fixes #974

--- js/algoliasearch/internals/frontend/common.js
+++ js/algoliasearch/internals/frontend/common.js
@@ -2,12 +2,12 @@ document.addEventListener("DOMContentLoaded", function (e) {
 	algoliaBundle.$(function ($) {
 		window.isMobile = function() {
 			var check = false;
-			
+
 			(function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
-			
+
 			return check;
 		};
-		
+
 		window.transformHit = function (hit, price_key, helper) {
 			if (Array.isArray(hit.categories)) {
 				hit.categories = hit.categories.join(', ');
@@ -20,19 +20,19 @@ document.addEventListener("DOMContentLoaded", function (e) {
 
 				hit.categories_without_path = hit.categories_without_path.join(', ');
 			}
-			
+
 			var matchedColors = [];
-			
+
 			if (helper && algoliaConfig.useAdaptiveImage === true) {
 				if (hit.images_data && helper.state.facetsRefinements.color) {
 					matchedColors = helper.state.disjunctiveFacetsRefinements.color.slice(0); // slice to clone
 				}
-				
+
 				if (hit.images_data && helper.state.disjunctiveFacetsRefinements.color) {
 					matchedColors = helper.state.disjunctiveFacetsRefinements.color.slice(0); // slice to clone
 				}
 			}
-			
+
 			if (Array.isArray(hit.color)) {
 				var colors = [];
 
@@ -40,17 +40,17 @@ document.addEventListener("DOMContentLoaded", function (e) {
 					if (color.matchLevel === 'none') {
 						return;
 					}
-					
+
 					colors.push(color.value);
-					
+
 					if (algoliaConfig.useAdaptiveImage === true) {
 						var re = /<em>(.*?)<\/em>/g;
 						var matchedWords = color.value.match(re).map(function (val) {
 							return val.replace(/<\/?em>/g, '');
 						});
-						
+
 						var matchedColor = matchedWords.join(' ');
-						
+
 						if (hit.images_data && color.fullyHighlighted && color.fullyHighlighted === true) {
 							matchedColors.push(matchedColor);
 						}
@@ -64,11 +64,11 @@ document.addEventListener("DOMContentLoaded", function (e) {
 			else if (hit._highlightResult.color && hit._highlightResult.color.matchLevel === 'none') {
 				hit._highlightResult.color = { value: '' };
 			}
-			
+
 			if (algoliaConfig.useAdaptiveImage === true) {
 				$.each(matchedColors, function (i, color) {
 					color = color.toLowerCase();
-					
+
 					if (hit.images_data[color]) {
 						hit.image_url = hit.images_data[color];
 						hit.thumbnail_url = hit.images_data[color];
@@ -94,6 +94,16 @@ document.addEventListener("DOMContentLoaded", function (e) {
 
 			if (hit['price'] !== undefined && price_key !== '.' + algoliaConfig.currencyCode + '.default' && hit['price'][algoliaConfig.currencyCode][price_key.substr(1) + '_formated'] !== hit['price'][algoliaConfig.currencyCode]['default_formated']) {
 				hit['price'][algoliaConfig.currencyCode][price_key.substr(1) + '_original_formated'] = hit['price'][algoliaConfig.currencyCode]['default_formated'];
+			}
+
+			// Expire the special_price if needed.
+      if (hit['price'][algoliaConfig.currencyCode]['special_to_date']) {
+				var special_to_date = hit['price'][algoliaConfig.currencyCode]['special_to_date'];
+				if (algoliaConfig.current_timestamp > special_to_date) {
+          var default_original_formated = hit['price'][algoliaConfig.currencyCode]['default_original_formated']
+          hit['price'][algoliaConfig.currencyCode]['default_original_formated'] = false;
+          hit['price'][algoliaConfig.currencyCode]['default_formated'] = default_original_formated;
+				}
 			}
 
 			return hit;
@@ -193,7 +203,7 @@ document.addEventListener("DOMContentLoaded", function (e) {
 				var suggestions_index = algolia_client.initIndex(algoliaConfig.indexName + "_suggestions"),
 					products_index = algolia_client.initIndex(algoliaConfig.indexName + "_products"),
 					suggestionsSource;
-				
+
 				if (algoliaConfig.autocomplete.displaySuggestionsCategories == true) {
 					suggestionsSource = $.fn.autocomplete.sources.popularIn(suggestions_index, {
 						hitsPerPage: section.hitsPerPage
